c4ffef8647bf1bcdb7231cf43f56661f7eb7f869
Igor <igor.ssqwe@gmail.com>
2015-11-07 19:11:23 -0200

    Adicionando divisão de cores em hexadecimal

--- assets/js/form-color.js
+++ assets/js/form-color.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$('.getColor').keyup(function(){
 		var getColor = $('.getColor').val();
 		var setColor = $('.box-color').css('background-color');
+		var colorLength = getColor.length;
 
 		$('.box-color').css('background-color', getColor);
 		$('.setColor').val(setColor);
@@ -10,6 +11,19 @@ $(document).ready(function(){
 			$('.box-color').css('background-color', '#fff');
 			$('.setColor').val('');
 		};
-		
+		if (colorLength == 7){
+			var red = getColor.substring(1, 3);
+			var redBox = '#' + red + "0000";
+			$('.red').css('background-color', redBox);
+
+			var green = getColor.substring(3, 5);
+			var greenBox = '#00' + green + "00";
+			$('.green').css('background-color', greenBox);
+
+			var blue = getColor.substring(5, 7);
+			var blueBox = '#0000' + blue;
+			$('.blue').css('background-color', blueBox);
+		};
 	});
+
 });
