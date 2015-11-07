0d6e77a787b21c29fad7e40cac964a54ec8eee9e
Igor <igor.ssqwe@gmail.com>
2015-11-07 19:39:24 -0200

    Adicionando divisão de cores em GRb (periodo de testes)

--- assets/js/form-color.js
+++ assets/js/form-color.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
 			$('.box-color').css('background-color', '#fff');
 			$('.setColor').val('');
 		};
+
 		if (colorLength == 7){
 			var red = getColor.substring(1, 3);
 			var redBox = '#' + red + "0000";
@@ -24,6 +25,19 @@ $(document).ready(function(){
 			var blueBox = '#0000' + blue;
 			$('.blue').css('background-color', blueBox);
 		};
-	});
 
+		if (colorLength == 16){
+			var red = getColor.substring(4, 7);
+			var redBox = 'rgb(' + red + ",000,000)";
+			$('.red').css('background-color', redBox);
+
+			var green = getColor.substring(8, 11);
+			var greenBox = 'rgb(000,' + green + ",000)";
+			$('.green').css('background-color', greenBox);
+
+			var blue = getColor.substring(4, 7);
+			var blueBox = 'rgb(000,000,' + blue + ")";
+			$('.blue').css('background-color', blueBox);
+		};
+	});
 });
