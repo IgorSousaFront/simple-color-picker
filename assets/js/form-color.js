129aae071cc57f4a03f57eb35991e5ffebc43a04
Igor <igor.ssqwe@gmail.com>
2015-11-07 22:15:11 -0200

    Ajustes no sistema de divisão de cores

--- assets/js/form-color.js
+++ assets/js/form-color.js
@@ -1,44 +1,31 @@
 $(document).ready(function(){
 	$('.getColor').keyup(function(){
-		var getColor = $('.getColor').val();
-		var setColor = $('.box-color').css('background-color');
-		var colorLength = getColor.length;
+		var getColor     = $('.getColor').val();
+		var setColor     = $('.box-color').css('background-color');
+		var colorLength  = setColor.length;
+		console.log(colorLength);
+		var textColor    = $('.result-content p');
+		// var hexa         = [0, 1, 2, 3 , 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+
 
 		$('.box-color').css('background-color', getColor);
 		$('.setColor').val(setColor);
 
-
-		if (colorLength == 7){
-			var red = getColor.substring(1, 3);
-			var redBox = '#' + red + "0000";
-			$('.red').css('background-color', redBox);
-
-			var green = getColor.substring(3, 5);
-			var greenBox = '#00' + green + "00";
-			$('.green').css('background-color', greenBox);
-
-			var blue = getColor.substring(5, 7);
-			var blueBox = '#0000' + blue;
-			$('.blue').css('background-color', blueBox);
-		}else{
-			
-		};
-
-		if (colorLength == 16){
-			var red = getColor.substring(4, 7);
+		if (colorLength > 15){
+			var red = setColor.substring(4, 7);
 			var redBox = 'rgb(' + red + ",000,000)";
 			$('.red').css('background-color', redBox);
 
-			var green = getColor.substring(8, 11);
+			var green = setColor.substring(8, 11);
 			var greenBox = 'rgb(000,' + green + ",000)";
 			$('.green').css('background-color', greenBox);
 
-			var blue = getColor.substring(4, 7);
+			var blue = setColor.substring(4, 7);
 			var blueBox = 'rgb(000,000,' + blue + ")";
 			$('.blue').css('background-color', blueBox);
 		};
 
-		if (getColor == 0) {
+		if (getColor.length <= 1) {
 			$('.box-color').css('background-color', '#fff');
 			$('.setColor').val('');
 			$('.result').css('background-color', '#fff');
