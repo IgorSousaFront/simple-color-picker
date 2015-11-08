ac87b1d6e82288ee45e29f95ae01c3229ec856d2
Igor <igor.ssqwe@gmail.com>
2015-11-07 22:19:56 -0200

    Sistema de divisão de cores (correções).

--- assets/js/form-color.js
+++ assets/js/form-color.js
@@ -3,9 +3,7 @@ $(document).ready(function(){
 		var getColor     = $('.getColor').val();
 		var setColor     = $('.box-color').css('background-color');
 		var colorLength  = setColor.length;
-		console.log(colorLength);
 		var textColor    = $('.result-content p');
-		// var hexa         = [0, 1, 2, 3 , 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
 
 		$('.box-color').css('background-color', getColor);
